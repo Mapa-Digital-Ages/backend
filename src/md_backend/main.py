8d0ca7e2b0784baca0de44ab37a130d661b09f1c
GuilhermeGMV <gui141202@gmail.com>
2026-04-12 19:19:29 -0300

    add CORS middleware to allow requests from localhost

--- src/md_backend/main.py
+++ src/md_backend/main.py
@@ -3,6 +3,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from md_backend.routes.router import router
 from md_backend.utils.database import init_db
@@ -16,6 +17,14 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
