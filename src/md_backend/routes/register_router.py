486eaa18fc6cf3e75e9f1aaa4eaafee4401178df
Nicolas Docolas <nicolas.docolas1804@gmail.com>
2026-04-30 22:29:54 -0300

    refactor(register): rename endpoints and accept first_name/last_name/phone_number

--- src/md_backend/routes/register_router.py
+++ src/md_backend/routes/register_router.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends, status
 from fastapi.responses import JSONResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from md_backend.models.api_models import AlunoRegisterRequest, RegisterRequest
+from md_backend.models.api_models import RegisterRequest, StudentRegisterRequest
 from md_backend.services.register_service import RegisterService
 from md_backend.utils.database import get_db_session
 
@@ -12,13 +12,18 @@ register_service = RegisterService()
 register_router = APIRouter(prefix="/register")
 
 
-@register_router.post("/responsavel")
-async def register_responsavel(
+@register_router.post("/guardian")
+async def register_guardian(
     request: RegisterRequest, session: AsyncSession = Depends(get_db_session)
 ):
-    """Register a new user."""
-    result = await register_service.register_responsavel(
-        email=request.email, password=request.password, name=request.name, session=session
+    """Register a new guardian user."""
+    result = await register_service.register_guardian(
+        email=request.email,
+        password=request.password,
+        first_name=request.first_name,
+        last_name=request.last_name,
+        phone_number=request.phone_number,
+        session=session,
     )
 
     if result is None:
@@ -28,17 +33,20 @@ async def register_responsavel(
         )
     return JSONResponse(content=result, status_code=status.HTTP_201_CREATED)
 
-@register_router.post("/aluno")
-async def register_aluno(
-    request: AlunoRegisterRequest, session: AsyncSession = Depends(get_db_session)
+@register_router.post("/student")
+async def register_student(
+    request: StudentRegisterRequest, session: AsyncSession = Depends(get_db_session)
 ):
-    """Register a new user."""
-    result = await register_service.register_aluno(
+    """Register a new student user."""
+    result = await register_service.register_student(
         email=request.email,
         password=request.password,
-        name=request.name,
+        first_name=request.first_name,
+        last_name=request.last_name,
+        phone_number=request.phone_number,
         birth_date=request.birth_date,
         student_class=request.student_class,
+        school_id=request.school_id,
         session=session,
     )
 
